b506262da1f606dcaa0243bf9b6d99d04ba52bc2
sanbo.dev <sanbo.dev@gmail.com>
2015-12-27 13:42:40 +0000

    add sound

--- index.js
+++ index.js
@@ -4,16 +4,24 @@
 // require
 var Bt = require("bleacon");
 var Gpio = require("onoff").Gpio;
+var exec = require('child_process').exec;
+
 var led_red    = new Gpio(23, 'out');	// pin no : 16
 var led_yellow = new Gpio(24, 'out');	// pin no : 18
 var led_green  = new Gpio(25, 'out');	// pin no : 22
 
+var arrayTimerID = [];
 var px = '';
+var soundFlg = false;
 
 
 //
 // inner function
 //
+function initialize() {
+	exec('hciconfig hci0 up', function() {});
+};
+
 function exit() {
 	// All LED turn OFF
 	led_red.writeSync(0);
@@ -30,6 +38,8 @@ function exit() {
 //
 // scan start
 //
+initialize();
+
 console.log("start scanning");
 Bt.startScanning();
 
@@ -47,21 +57,34 @@ Bt.on("discover", function(bleacon) {
 	//console.log(bleacon.proximity);
 	px = bleacon.proximity;
 
+	var resetTimerFlg = false;
+
 	switch (px) {
 		case 'immediate':
 			led_red.writeSync(0);
 			led_yellow.writeSync(0);
 			led_green.writeSync(1);
+			resetTimerFlg = true;
+
+			var txt = 'ここはきけんです';
+			soundFlg = true;
+			exec('./jsay.sh ' + txt, function() {
+				setTimeout(function() {
+					soundFlg = false;
+				}, 3000);
+			});
 			break;
 		case 'near':
 			led_red.writeSync(0);
 			led_yellow.writeSync(1);
 			led_green.writeSync(0);
+			resetTimerFlg = true;
 			break;
 		case 'far':
 			led_red.writeSync(1);
 			led_yellow.writeSync(0);
 			led_green.writeSync(0);
+			resetTimerFlg = true;
 			break;
 		case 'unknown':
 			led_red.writeSync(1);
@@ -74,6 +97,19 @@ Bt.on("discover", function(bleacon) {
 			led_green.writeSync(0);
 			break;
 	}
+
+	// timer clear
+	if (resetTimerFlg == true && arrayTimerID.length > 0) {
+		clearTimeout(arrayTimerID.shift());
+	}
+
+	// set Timer
+	arrayTimerID.push( setTimeout(function() {
+		led_red.writeSync(0);
+		led_yellow.writeSync(0);
+		led_green.writeSync(0);
+		arrayTimerID.shift();
+	}, 5000));
 
 });
 
