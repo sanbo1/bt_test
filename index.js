2f237d280630eebd29779be4c32087ba07cae051
sanbo.dev <sanbo.dev@gmail.com>
2015-12-27 14:14:27 +0000

    modify index.js

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ var led_red    = new Gpio(23, 'out');	// pin no : 16
 var led_yellow = new Gpio(24, 'out');	// pin no : 18
 var led_green  = new Gpio(25, 'out');	// pin no : 22
 
+//var uuid = "95F428B1-4A3A-4E39-B086-21BFF38DEB6D";	// NG
+//var uuid = "95F428B14A3A4E39B08621BFF38DEB6D";		// NG
+//var uuid = "95f428b1-4a3a-4e39-b086-21bff38deb6d";	// NG
+var uuid = "95f428b14a3a4e39b08621bff38deb6d";			// ハイフンなし、小文字 でないと認識しない
+var major = 440;
+var minor = 287;
+
 var arrayTimerID = [];
 var px = '';
 var soundFlg = false;
@@ -41,7 +48,10 @@ function exit() {
 initialize();
 
 console.log("start scanning");
-Bt.startScanning();
+//Bt.startScanning();
+//Bt.startScanning(uuid);
+//Bt.startScanning(uuid, major);
+Bt.startScanning(uuid, major, minor);
 
 
 //
@@ -50,41 +60,50 @@ Bt.startScanning();
 Bt.on("discover", function(bleacon) {
 	//console.log(bleacon);
 
+	// timer clear
+	if (arrayTimerID.length > 0) {
+		clearTimeout(arrayTimerID.shift());
+	}
+
+	// set Timer
+	arrayTimerID.push( setTimeout(function() {
+		led_red.writeSync(0);
+		led_yellow.writeSync(0);
+		led_green.writeSync(0);
+		arrayTimerID.shift();
+	}, 5000));
+
 	if (px == bleacon.proximity) {
 		return;
 	}
 
+	//console.log(bleacon.uuid);
 	//console.log(bleacon.proximity);
 	px = bleacon.proximity;
-
-	var resetTimerFlg = false;
 
 	switch (px) {
 		case 'immediate':
 			led_red.writeSync(0);
 			led_yellow.writeSync(0);
 			led_green.writeSync(1);
-			resetTimerFlg = true;
 
-			var txt = 'ここはきけんです';
-			soundFlg = true;
-			exec('./jsay.sh ' + txt, function() {
-				setTimeout(function() {
+			if (soundFlg == false) {
+				var txt = 'ここはきけんです';
+				soundFlg = true;
+				exec('./jsay.sh ' + txt, function() {
 					soundFlg = false;
-				}, 3000);
-			});
+				});
+			}
 			break;
 		case 'near':
 			led_red.writeSync(0);
 			led_yellow.writeSync(1);
 			led_green.writeSync(0);
-			resetTimerFlg = true;
 			break;
 		case 'far':
 			led_red.writeSync(1);
 			led_yellow.writeSync(0);
 			led_green.writeSync(0);
-			resetTimerFlg = true;
 			break;
 		case 'unknown':
 			led_red.writeSync(1);
@@ -97,19 +116,6 @@ Bt.on("discover", function(bleacon) {
 			led_green.writeSync(0);
 			break;
 	}
-
-	// timer clear
-	if (resetTimerFlg == true && arrayTimerID.length > 0) {
-		clearTimeout(arrayTimerID.shift());
-	}
-
-	// set Timer
-	arrayTimerID.push( setTimeout(function() {
-		led_red.writeSync(0);
-		led_yellow.writeSync(0);
-		led_green.writeSync(0);
-		arrayTimerID.shift();
-	}, 5000));
 
 });
 
